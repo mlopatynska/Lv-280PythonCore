12b8d2218d763ff799bd861a1f4c9d7534a83b43
Andrii Berezhans'kyi <abereztc@training.local>
2017-11-07 20:33:42 +0200

    Homework

---
+++ Homework/Homework_1.1.py
@@ -0,0 +1,10 @@
+name = raw_input("What is your name?\n")
+
+age = raw_input("How old are you?\n")
+
+adress = raw_input("Where do you live?\n")
+
+print ""
+print "Hello", name
+print "You are", age, "years old"
+print "You live in", adress
---
+++ Homework/Homework_1.2.py
@@ -0,0 +1,9 @@
+a = input("Please input variable a (number)\n")
+
+b = input("Please input variable b (number)\n")
+
+print ""
+print "a =", a
+print "b =", b
+print "a + b =", a+b
+print "a - b =", a-b
