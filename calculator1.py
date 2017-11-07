a3a8540e438f52c12539e0999018deeceb2f6896
Rostyslav Novosiadlo <rnovotc@training.local>
2017-11-07 20:35:30 +0200

    blabla

---
+++ calculator1.py
@@ -0,0 +1,23 @@
+x = float(input("First number:"))
+y = float(input("Second number:"))
+operation = (input("operation:"))
+
+Result=None
+
+if operation == "+" :
+    result = x+y
+elif operation == "-" :
+    result = x-y
+elif operation == "*" :
+    result = x*y
+elif operation == "/" :
+    result= x/y
+elif operation == "%":
+    result = x%y
+elif operation == "//":
+    result = x//y
+else:
+    print ("Unsupported operation:")
+
+if result is not None :
+    print("Result",result)
