24b8e73a6f728527e2fc2ae264b63a0ed14f6953
marianmagur <marianmagur@gmail.com>
2017-11-08 15:29:04 +0200

    HomeWork 1

---
+++ hw1_2.py
@@ -0,0 +1,9 @@
+variable_x=int(raw_input('input x: '))
+variadle_y=int(raw_input('input y: '))
+print '\n'
+print variable_x ,'+', variadle_y,'=' ,(variable_x)+(variadle_y)
+print variable_x ,'-', variadle_y,'=' ,(variable_x)-(variadle_y)
+print variable_x ,'*', variadle_y,'=' ,(variable_x)*(variadle_y)
+print variable_x ,'/', variadle_y,'=' ,(float(variable_x))/(variadle_y)
+print variable_x ,'%', variadle_y,'=' ,(variable_x)%(variadle_y)
+print variable_x ,'**', variadle_y,'=' ,(variable_x)**(variadle_y)
