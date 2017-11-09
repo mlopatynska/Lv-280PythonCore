65d22acdfe1c93879db6b8cfa0540d2a03dc0e9d
MeriVerbovska <mariyalira@yahoo.com>
2017-11-09 17:25:17 +0200

    my homeWork

---
+++ HomeWork1/hw1.py
@@ -0,0 +1,6 @@
+name =raw_input("What is your name? ")
+years=raw_input("How old are you? ")
+city =raw_input("Where are you live? ")
+print "Hello,",name,"!"
+print "Your age is ",years,"."
+print "You live in ",city,"." 
---
+++ HomeWork1/hw2.py
@@ -0,0 +1,8 @@
+a=float(input("Input a="))
+b=float(input("Input b="))
+print "a+b=",a+b
+print "a-b=",a-b
+print "a/b=",a/b
+print "a*b=",a*b
+print "a%b=",int(a%b)
+print "a**b=",a**b
