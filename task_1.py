399d6f2f193e8a690d2ffffcd1f897f1575b3cc2
Stanislav Bronevytskyy <sbronetc@training.local>
2017-11-10 18:29:24 +0200

    hometask

---
+++ task_1.py
@@ -0,0 +1,7 @@
+a = raw_input("enter your name: ")
+b = raw_input("enter your age:  ")
+c = raw_input("enter city where you live: ")
+print("Hello," + " " + a)
+print("You are" + " " + b + " " + "years old")
+print ("You live in" + " " + c)
+
