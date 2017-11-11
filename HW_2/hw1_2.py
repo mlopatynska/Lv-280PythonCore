26eee9d6797be83247f05f938b7cd781d34d3d7e
marianmagur <marianmagur@gmail.com>
2017-11-11 17:18:12 +0200

    if U happy & U know it

---
+++ HW_2/hw1_2.py
@@ -0,0 +1,17 @@
+numb= 1825
+numb_became_str = str(numb)
+result=1
+ 
+for i in numb_became_str:
+ f=int(i)
+ result=result*f
+print 'product of numbers is:' , result
+# reverse
+print 'reverse of numbers is:' , numb_became_str[::-1]
+#sorted
+print "sorted numbers       :" , ''.join(sorted(numb_became_str))
+#changing variables
+x=22 
+y=7
+x,y=y,x
+print 'result of changing variables is :', x ,'&', y
