7257403aef3acadeafd3a21b7c92939a1fb35262
MeriVerbovska <mariyalira@yahoo.com>
2017-11-13 18:26:45 +0200

    My homeWork2

---
+++ HomeWork2/hw2_2.py
@@ -0,0 +1,12 @@
+num=8612
+#a
+num=str(num)
+print int(num[0])*int(num[1])*int(num[2])*int(num[3])
+#b
+num1=list(num)
+num1.reverse()
+print ''.join(num1)
+
+#c
+print ''.join(sorted(num))
+
---
+++ HomeWork2/hw2_3.py
@@ -0,0 +1,25 @@
+x=1
+y=2
+x=x+y
+y=x-y
+x=x-y
+print x
+print y
+
+x='word'
+y='live'
+x,y=y,x
+print x
+print y
+
+x=1
+y=2
+x,y=y,x
+print x
+print y
+
+x=[1]
+y=[2]
+x,y=y,x
+print x
+print y
