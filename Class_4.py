867422a21110a9e67a2a7ecfff84d2c6af26b8d0
aberezh <33362956+aberezh@users.noreply.github.com>
2017-11-17 19:43:52 +0200

    Class 4

---
+++ Class_4.py
@@ -0,0 +1,81 @@
+# 1
+# password = raw_input("Enter password\n")
+#
+# if len(password) >= 10:
+#     print ("Welcome")
+# else:
+#     print ("Password is too short")
+
+
+# 2.1
+# age = 12
+#
+# if age < 12:
+#     print('kid')
+# elif age < 18:
+#     print('teenager')
+# elif age < 50:
+#     print('adult')
+# else:
+#     print('you are not old')
+
+
+# 2.2
+# goods_number = input("How many items did you buy?\n")
+#
+# if goods_number >100:
+#     discount = "50%"
+# elif goods_number >50:
+#     discount = "10%"
+# elif goods_number >10:
+#     discount = "1%"
+# else:
+#     discount = "0%"
+#
+# print "Your discount is %s" % discount
+
+
+# 3.2
+# weather = "raining"
+#
+# print("Open Your umbrella" if weather == "raining" else "Wear your cap")
+
+
+# 3.3
+# a = input("a = ")
+# b = input("b = ")
+#
+# print ("a<b" if a<b else "a>b")
+
+
+# 4
+# product = raw_input("What do you want to buy?\n")
+# product_list = ["milk", "sugar", "bread", "salt"]
+#
+# if product in product_list:
+#     print ("The product is available")
+# else:
+#     print ("We ran out of stock")
+
+
+# 5.1
+# number = input("Input number\n")
+#
+# if (number > 0 and number < 10) or (number > 100 and number < 110):
+#     print ("You won the lottery")
+# else:
+#     print ("You lost")
+
+
+# 5.2
+# number = input("Input number\n")
+#
+# if (number in range(11)) or number in range(100, 111):
+#     print ("You won the lottery")
+# else:
+#     print ("You lost")
+
+
+# 5.3
+# number = input("Enter number\n")
+# print ("It is even" if number%2==0 else "It is uneven")
