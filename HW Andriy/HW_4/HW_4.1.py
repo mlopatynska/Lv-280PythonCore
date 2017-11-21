4aa0bb89d8831adba5b88e9b5a9baa00f72837fc
aberezh <berezhanskyya@gmail.com>
2017-11-21 03:58:04 +0200

    Change in HW 4.1

--- HW Andriy/HW_4/HW_4.1.py
+++ HW Andriy/HW_4/HW_4.1.py
@@ -1,6 +1,10 @@
 year = input("Please enter year\n")
 
-if year % 4 == 0:
+print ""
+
+if year % 100 == 0 and year % 400 != 0:
+    print "It's not a leap year!"
+elif year % 4 == 0:
     print "It's a leap year!"
 else:
     print "It's not a leap year!"
