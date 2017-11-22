c0dd5bd0d899f1c450869faa0e5c69523f6ce269
sergvh@gmai.com <RjhdfkjK123>
2017-11-23 00:28:49 +0200

    4 digits sum.

---
+++ hw4/4_1.py
@@ -0,0 +1,15 @@
+num = input("Enter number please:")
+for i in range(1000, 9999, 151):
+    sum_ = 0
+    count = 0
+    result = i
+    while i > 0:
+        if i % 10 != 0:
+            sum_ = sum_ + (i % 10)
+        i = i // 10
+    if sum_ == num:
+        count += 1
+        print("{} is equal sum of {} digits".format(num, result))
+
+if count == 0:
+    print ("{} is not equal to any sum of digits".format(num))
