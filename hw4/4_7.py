821a03c3e69bf97f4ee637714574a5b93d1708ad
sergvh@gmai.com <RjhdfkjK123>
2017-12-04 21:02:12 +0200

    Letters count ref

--- hw4/4_7.py
+++ hw4/4_7.py
@@ -1,8 +1,10 @@
+#!/usr/bin/python
+# -*- coding: utf8 -*-
 mystr=raw_input("Enter some string please:")
 mylist={}
 for i in range(len(mystr)):
-    if mystr[i] not in mylist:
-        mylist[mystr[i]]=mystr.count(mystr[i])
+    #if mystr[i] not in mylist:
+    mylist[mystr[i]]=mystr.count(mystr[i])
 
 
 print mylist
