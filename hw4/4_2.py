c0a7b4ac87c7d9d3e6952a2fc5fb648fdf6dd07b
sergvh@gmai.com <RjhdfkjK123>
2017-12-04 21:00:31 +0200

    Letters count ref

--- hw4/4_2.py
+++ hw4/4_2.py
@@ -4,6 +4,6 @@ for i in range(1,num,1):
     if num%i == 0:
         count +=1
 if count>2:
-    print("{} this number si NOT prime number".format(num))
+    print("{} this number is NOT prime number".format(num))
 else:
-    print("{} this number si PRIME NUMBER".format(num))
+    print("{} this number is PRIME NUMBER".format(num))
