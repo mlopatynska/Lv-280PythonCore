c4c3c1af3a10a077dbca111360acc7593021d47f
MeriVerbovska <mariyalira@yahoo.com>
2017-12-11 20:07:50 +0200

    This is my game

--- game.py
+++ game.py
@@ -1,110 +1,174 @@
-import pygame, datetime, random
+import pygame, time, random
 pygame.init()
 
 done = False
 clock = pygame.time.Clock()
-n=6
-screen = pygame.display.set_mode((n*100,n*100))
+N=6
+
+screen = pygame.display.set_mode((N*100,N*100))
+
 pygame.display.set_caption('My first game')
+font = pygame.font.SysFont('Calibri', N*15, True, False)
 
-AQUA = (0,255,255)
+COLOR = [(128,128,128),(0,255,0),(255,0,0),(0,0,255)]
+
+#AQUA = (0,255,255)
 BLACK = (0,0,0)
-BLUE = (0,0,255)
-PURPLE = (128,0,128)
-RED = (255,0,0)
-GREY = (128,128,128)
-GREEN = (0,255,0)
+#BLUE = (0,0,255)
+#PURPLE = (128,0,128)
+#RED = (255,0,0)
+#GREY = (128,128,128)
+#GREEN = (0,255,0)
 WHITE = (255,255,255)
-YELLOW = (255,255,0)
+#YELLOW = (255,255,0)
 
 
+SCORE = 2000
+LEVEL = 50000
+field = [[0 for i in range(N)] for j in range(N)]
+last_time=0
+level_time=0
 
 
-def boll(cell,color):
-    pygame.draw.circle(screen,color,cell,40)
+def boll(i,j,c):
+    pygame.draw.circle(screen,COLOR[c],[field[i][j][0],field[i][j][1]],40)
+    field[i][j][2]=c
 
-def square(cell,color):
-    pygame.draw.rect(screen,color,[cell[0]-50,cell[1]-50,97,97])
+def square(i,j,c=COLOR[0]):
+    pygame.draw.rect(screen,c,[field[i][j][0]-50,field[i][j][1]-50,97,97])
+    field[i][j][2]=0
 
-def find(x,y):
-    pxarray = pygame.PixelArray(screen)
-    if pxarray[field[x][y][0],field[x][y][1]] == screen.map_rgb(GREEN):
-        print 'GREEN'
-    else: print 'not'
+def emptyfield():
+    screen.fill(BLACK)
+    for i in range(N):
+        for j in range(N):
+            field[i][j]=[50+(i*100),50+(j*100),0]
+            square(i,j)
+    
+def findline(i,j):
+    if i-2>=0:    
+        if field[i-2][j][2] == field[i-1][j][2] == field[i][j][2]:
+            square((i-2),j)
+            square((i-1),j)
+            square(i,j)
+    if i-1>=0 and i+1<N:
+        if field[i-1][j][2] == field[i][j][2] == field[i+1][j][2]:
+            square(i-1,j)
+            square(i,j)
+            square(i+1,j)
+    if i+2<N:
+        if field[i][j][2] == field[i+1][j][2] == field[i+2][j][2]:
+            square(i,j)
+            square(i+1,j)
+            square(i+2,j)
+    if j-2>=0:    
+        if field[i][j-2][2] == field[i][j-1][2] == field[i][j][2]:
+            square(i,j-2)
+            square(i,j-1)
+            square(i,j)
+    if j-1>=0 and j+1<N:
+        if field[i][j-1][2] == field[i][j][2] == field[i][j+1][2]:
+            square(i,j-1)
+            square(i,j)
+            square(i,j+1)
+    if j+2<N:
+        if field[i][j][2] == field[i][j+1][2] == field[i][j+2][2]:
+            square(i,j)
+            square(i,j+1)
+            square(i,j+2)
 
-def line():
-    k=0
-    for i in range(n):
-        for j in range(n):
-            if find(i,j)==True: k+=1
+def findover():
+    for i in range(N):
+        for j in range(N):
+            if field[i][j][2]==0:
+                return True
+    return False
 
-field = [[0 for i in range(n)] for j in range(n)]
-for i in range(n):
-    for j in range(n):
-        field[i][j]=[50+(i*100),50+(j*100)]
+def gameover():
+    text = font.render("GAME OVER", True, WHITE)
+    screen.blit(text, [70, 200])
+    
+
+#screen.fill(WHITE)
+#pygame.draw.rect(screen,GREEN,[n*20,m*25,n*50,m*20])
+#text = font.render("GAME", True, BLACK)
+#screen.blit(text, [n*30, n*40])
+#pygame.draw.rect(screen,GREEN,[n*60,m*25,n*50,m*20])
+#text = font.render("GAME OVER", True, BLACK)
+#screen.blit(text, [n*30, n*40])
 
 
+#------picture field------
+emptyfield()
 
-for i in range(n):
-    for j in range(n):
-        square(field[i][j],GREY)
-
-x=random.randint(0,5)
-y=random.randint(0,5)
-boll(field[x][y],GREEN)
-
-l_time=0
-
+x,y,z=random.randint(0,N-1),random.randint(0,N-1),random.randint(1,3)
+boll(x,y,z)
 
 
 while not done:
     for event in pygame.event.get():
-        clock.tick()
-
         if event.type == pygame.QUIT: 
-            done = True 
+            done = True
+            gameover() 
+            
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
-                if x-1>=0: x-= 1
-                else: x=x
-                boll(field[x][y],GREEN)
-                square(field[x+1][y],GREY)
+                if x-1>=0 and field[x-1][y][2] == 0:
+                    x-= 1
+                    boll(x,y,z)
+                    square((x+1),y)
+                
             elif event.key == pygame.K_RIGHT:
-                if x+1<n: x+= 1
-                else: x=x
-                boll(field[x][y],GREEN)
-                square(field[x-1][y],GREY)
+                if x+1<N and field[x+1][y][2] == 0:
+                    x+= 1
+                    boll(x,y,z)
+                    square((x-1),y)
+                
             elif event.key == pygame.K_DOWN:
-                if y+1<n: y+= 1
-                else: y=y
-                boll(field[x][y],GREEN)
-                square(field[x][y-1],GREY)
+                if y+1<N and field[x][y+1][2] == 0:
+                    y+= 1
+                    boll(x,y,z)
+                    square(x,(y-1))
+                
             elif event.key == pygame.K_UP:
-                if y-1>=0: y -= 1
-                else: y=y
-                boll(field[x][y],GREEN)
-                square(field[x][y+1],GREY)
+                if y-1>=0 and field[x][y-1][2] == 0:
+                    y -= 1
+                    boll(x,y,z)
+                    square(x,(y+1))
+                
 
             
-        elif event.type == pygame.KEYUP:
-            print x,y
+#        elif event.type == pygame.KEYUP:
+#            print x,y
 #        elif event.type == pygame.MOUSEBUTTONDOWN:
 #            print("User pressed a mouse button")
 
 	
-#    screen.fill(WHITE)
-    if pygame.time.get_ticks() - l_time > 3000:
-        l_time = pygame.time.get_ticks()    
-        x=random.randint(0,5)
-        y=random.randint(0,5)
-        boll(field[x][y],GREEN)
-        find(x,y)
+
+    if pygame.time.get_ticks() - last_time > SCORE:
+            
+        x,y,z=random.randint(0,N-1),random.randint(0,N-1),random.randint(1,3)
+        if field[x][y][2]==0:
+            last_time = pygame.time.get_ticks()
+            boll(x,y,z)
+        else:
+            if findover()==False: 
+                done = True
+                gameover() 
+        if last_time-level_time>LEVEL:
+            level_time=last_time
+            SCORE-=500
+
+#            text = font.render("LEVEL "+str(SCORE/500-(SCORE/500-1)), True, WHITE)
+#            screen.blit(text, [200, 300])            
+#            emptyfield()   
+    findline(x,y)
 
 
-
+    
     pygame.display.update()
-    clock.tick(60)
+    clock.tick(10)
 
-
+time.sleep(2)
 pygame.quit()
 quit()
